e4a2f196df42cd6b6f0fe219684efe7d5f51d523
Thales Testoni <thalestestoni@gmail.com>
2021-11-14 16:48:22 -0300

    useInfinityQuery and handle loading or error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,24 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type FetchImagesResponse = {
+  after?: string;
+  data: {
+    ts: number,
+    id: string;
+    title: string;
+    description: string;
+    url: string;
+  }[]
+}
+
 export default function Home(): JSX.Element {
+  const fetchImages = async ({ pageParam = null }) => {
+    return await api.get<FetchImagesResponse>('/api/images', { 
+      params: { after: pageParam }
+    }).then(response => response.data)
+  }
+
   const {
     data,
     isLoading,
@@ -18,27 +35,28 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
+    fetchImages, 
+    {
+      getNextPageParam: (lastPage) => lastPage.after ?? null
+    }
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  return isLoading ? (
+    <Loading />
+  ) : isError ? (
+    <Error />
+  ) : (
+      <>
+        <Header />
 
-  // TODO RENDER ERROR SCREEN
-
-  return (
-    <>
-      <Header />
-
-      <Box maxW={1120} px={20} mx="auto" my={20}>
-        <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
-      </Box>
-    </>
-  );
+        <Box maxW={1120} px={20} mx="auto" my={20}>
+          <CardList cards={formattedData} />
+          {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        </Box>
+      </>
+    );
 }
