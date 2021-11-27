b83f8561965ba66a7e82eda2633687f8cf5e3a91
Thales Testoni <thalestestoni@gmail.com>
2021-11-27 10:00:42 -0300

    Submithandler upload images

--- src/components/Form/FormAddImage.tsx
+++ src/components/Form/FormAddImage.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, Stack, useToast } from '@chakra-ui/react';
-import { useForm } from 'react-hook-form';
-import { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import React, { useState } from 'react';
 import { useMutation, useQueryClient } from 'react-query';
 
 import { api } from '../../services/api';
@@ -92,15 +92,34 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
   } = useForm();
   const { errors } = formState;
 
-  const onSubmit = async (data: Record<string, unknown>): Promise<void> => {
+  const onSubmit: SubmitHandler<UploadImageFormData> = async (data): Promise<void> => {
     try {
-      // TODO SHOW ERROR TOAST IF IMAGE URL DOES NOT EXISTS
-      // TODO EXECUTE ASYNC MUTATION
-      // TODO SHOW SUCCESS TOAST
+      if (!imageUrl) {
+        toast({
+          status: 'info',
+          title: 'Imagem não adicionada',
+          description: 'É preciso adicionar e aguardar o upload de uma imagem antes de realizar o cadastro.'
+        })
+      }
+
+      await mutation.mutateAsync(data);
+      
+      toast({ 
+        status: 'success',
+        title: 'Imagem cadastrada',
+        description: 'Sua imagem foi cadastrada com sucesso.'
+      })
     } catch {
-      // TODO SHOW ERROR TOAST IF SUBMIT FAILED
+      toast({
+        status: 'error',
+        title: 'Falha no cadastro',
+        description: 'Ocorreu um erro ao tentar cadastrar a sua imagem.'
+      })
     } finally {
-      // TODO CLEAN FORM, STATES AND CLOSE MODAL
+      reset();
+      setImageUrl('');
+      setLocalImageUrl('');
+      closeModal();
     }
   };
 
@@ -115,21 +134,21 @@ export function FormAddImage({ closeModal }: FormAddImageProps): JSX.Element {
           trigger={trigger}
           name="image"
           onChange={async () => {}}
-          error={formState.errors.image}
+          error={errors.image}
           {...register('image', formValidations.image)}
         />
 
         <TextInput
           placeholder="Título da imagem..."
           name="fileName"
-          error={formState.errors.title}
+          error={errors.title}
           {...register('title', formValidations.title)}
         />
 
         <TextInput
           placeholder="Descrição da imagem..."
           name="fileDescription"
-          error={formState.errors.description}
+          error={errors.description}
           {...register('description', formValidations.description)}
         />
       </Stack>
